55ac43ae6d2bd8abe5341933da8e10fc40b533a0
Francesco Michelini <hello@francescomichelini.com>
2025-10-04 09:28:40 +0200

    Make points round

--- src/assets/Debug.js
+++ src/assets/Debug.js
@@ -1,8 +1,9 @@
 import { Pane } from 'tweakpane'
 
-import { pointRadius, pointStrength } from '@/assets/materials/SpiralMaterial'
+import { pointRadius, pointStrength, pointsSpeed } from '@/assets/materials/SpiralMaterial'
 
 const pane = new Pane()
 
 pane.addBinding(pointRadius, 'value', { label: 'pointRadius', min: 0, max: 6, step: 0.01 })
 pane.addBinding(pointStrength, 'value', { label: 'pointStrength', min: 0, max: 3, step: 0.01 })
+pane.addBinding(pointsSpeed, 'value', { label: 'pointsSpeed', min: 0, max: 2, step: 0.01 })
--- src/assets/materials/SpiralMaterial.js
+++ src/assets/materials/SpiralMaterial.js
@@ -1,20 +1,27 @@
-import { Fn, vec3, positionLocal, positionGeometry, positionWorld, uniform, attribute, modelWorldMatrix, float, min, color, smoothstep, time, cos, sin } from 'three/tsl'
+import { PI, Fn, vec3, positionLocal, positionGeometry, positionWorld, uniform, attribute, modelWorldMatrix, float, min, color, smoothstep, time, cos, sin } from 'three/tsl'
 import { MeshBasicNodeMaterial } from 'three/webgpu'
 
 export const SpiralMaterial = new MeshBasicNodeMaterial()
 
-export const pointRadius = uniform(2.5)
+export const pointRadius = uniform(4)
 export const pointStrength = uniform(2.5)
+export const pointsSpeed = uniform(1)
 
-const pointA = vec3()
-const pointB = vec3()
+const pointA = vec3().toVar()
+const pointB = vec3().toVar()
 
 const iPosition = attribute( 'a_InstancePosition', 'vec3' )
 
 SpiralMaterial.positionNode = Fn(() => {
-  // const xA = cos(time)
-  // const yA = sin(time)
-  // pointA.assign(vec3(xA, 0, yA))
+  const t = time.mul(pointsSpeed)
+
+  const xA = cos(t).mul(pointRadius)
+  const yA = sin(t).mul(pointRadius)
+  pointA.assign(vec3(xA, 0, yA))
+
+  const xB = cos(t.add(PI)).mul(pointRadius)
+  const yB = sin(t.add(PI)).mul(pointRadius)
+  pointB.assign(vec3(xB, 0, yB))
 
   const d = pointA.distance(modelWorldMatrix.mul(iPosition))
   const d2 = pointB.distance(modelWorldMatrix.mul(iPosition))
